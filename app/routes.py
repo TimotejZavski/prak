1aba661e0e8a68e8b6b01de0e824a52eaec6adaf
TimotejZavski <timotej.zavski@tutanota.com>
2024-02-26 12:19:59 +0100

    drugacna strukture. Zdej je se dela s system variabli in 2|3 fajli

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,15 @@ from flask import render_template, request, redirect, url_for
 import os
 import json
 import csv
+from openai import OpenAI
+import json
+import re
+import subprocess
+import sys
+import glob
+import time
+import subprocess
+import tiktoken
 
 
 UPLOAD_FOLDER = 'uploads'
@@ -30,20 +39,41 @@ def upload_file():
         filename = file.filename
         file.save(os.path.join(UPLOAD_FOLDER, filename))
 
-    
         csv_to_json(f'{os.path.join(UPLOAD_FOLDER, filename)}', f'{json_f_path}')
 
-
+        first_two_rows_json = get_first_two_rows(json_f_path)
 
         text1 = request.form['text1']
         text2 = request.form['text2']
+        os.environ['DESCRIPTION'] = text1
+        os.environ['TASK'] = text2
+        os.environ['JSONFPATH'] = json_f_path
+        os.environ['DATABLOCKEXAMPLE'] = first_two_rows_json
 
-        html_table = csv_to_html_table(f'{os.path.join(UPLOAD_FOLDER, filename)}')
-        # Process the file and texts here
-        return f'File "{filename}" uploaded successfully. Text 1: {text1}, Text 2: {text2}\n{html_table}'
+        process = subprocess.Popen(['python', '/Users/timzav/Desktop/prak/print.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        # Wait for the process to finish
+        stdout, stderr = process.communicate()
+
+        # Check return code to see if the script ran successfully
+        if process.returncode == 0:
+            print("Script execution successful.")
+        else:
+            print("Script execution failed.")
+
+        image_folder = '/Users/timzav/Desktop/prak/static/images'
+    
+        # Get a list of all files in the folder
+        image_files = os.listdir(image_folder)
+        
+        # Filter out non-image files
+        image_files = [file for file in image_files if file.endswith(('.jpg', '.png', '.gif', '.jpeg'))]
+        
+        # Render the HTML template and pass the image files to it
+        return render_template('upload.html', image_files=image_files)
     return 'Invalid file'
 
-
+# Convert CSV to JSON
 def csv_to_json(csv_file, json_file):
     with open(csv_file, 'r') as csvfile:
         csvreader = csv.DictReader(csvfile)
@@ -51,9 +81,6 @@ def csv_to_json(csv_file, json_file):
     with open(json_file, 'w') as jsonfile:
         json.dump(data, jsonfile, indent=4)
 
-
-
-def csv_to_html_table(csv_file):
     # Read CSV file
     with open(csv_file, 'r') as csvfile:
         csvreader = csv.reader(csvfile)
@@ -66,3 +93,29 @@ def csv_to_html_table(csv_file):
             html_table += "</tr>"
         html_table += "</table>"
     return html_table
+
+# first two rows of a JSON file
+def get_first_two_rows(json_file_path):
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+        # Assuming data is a list of dictionaries
+        first_two_rows = data[:2]
+        return json.dumps(first_two_rows, indent=4) 
+
+#ostale funkcije
+with open('/Users/timzav/Desktop/DataWizard/config.json') as f:
+    config = json.load(f)
+    kljuc = config['API_KEY']
+    client = OpenAI(api_key=kljuc)
+
+#file names list
+def files_list(folder_path):
+    file_names = []
+    if os.path.exists(folder_path):
+        for filename in os.listdir(folder_path):
+            if os.path.isfile(os.path.join(folder_path, filename)):
+                file_names.append(filename)
+    return file_names
+
+folder_path = '/images/'
+
---
+++ print.py
@@ -0,0 +1,151 @@
+from openai import OpenAI
+import json
+import re
+import subprocess
+import sys
+import os
+import glob
+import time
+import tiktoken
+
+#api
+with open('/Users/timzav/Desktop/DataWizard/config.json') as f:
+    config = json.load(f)
+    kljuc = config['API_KEY']
+    client = OpenAI(api_key=kljuc)
+
+
+
+"""manjka def tokens_in():"""#mora bit za not, ven in skupaj
+
+def tokens_in(Task, context):
+    total_tokens = count_tokens(Task + context)
+    return total_tokens
+
+def tokens_out(input_string, token_length=4):
+    count = 0
+    for _ in range(0, len(input_string), token_length):
+        count += 1
+    return count
+
+def count_tokens(input_string):
+    total_tokens = tiktoken.count_tokens(input_string)
+    return total_tokens
+
+st = 0#za napake stet, kasnej break
+zacasen = ""# ce je napaka je to task
+
+#file names list
+def files_list(folder_path):
+    file_names = []
+    if os.path.exists(folder_path):
+        for filename in os.listdir(folder_path):
+            if os.path.isfile(os.path.join(folder_path, filename)):
+                file_names.append(filename)
+    return file_names
+
+
+def skupek_imen():
+    file_names_list = files_list(folder_path)#iz funkcije
+    file_names_string = ' '.join(file_names_list)
+    return file_names_string
+description = os.environ.get('DESCRIPTION')
+Task = os.environ.get('TASK')
+JsonfPath = os.environ.get('JSONFPATH')
+jsonString = os.environ.get('DATABLOCKEXAMPLE')
+
+
+
+jsonFolder = '/Users/timzav/Desktop/prak/uploads'
+
+inputs = f"EXAMPLE OF DATA FROM JSON FILE:'{jsonString}', DESCRIPTION:'{description}', FILE PATH:'{JsonfPath}'"
+                   
+
+folder_path = '/Users/timzav/Desktop/prak/static/images'
+context = f'''Respond with code ONLY simple and plain Python code, no comments/explanations. Result of your code must always be generated png images of charts based on given:
+  {inputs}, COLLOR PALLETE of chart must be:'monochrome blue palettes', RESOLUTION of chart(png image) must be:1920x1080
+  Save the charts as high-resolution PNG images to the '{folder_path}'.
+  '''
+
+
+while True:
+    if st <= 4:#3 krat lahka nardi napako
+        #s_time = time.time()
+        response = client.chat.completions.create(
+            model="gpt-4-1106-preview", 
+            messages=[
+                {"role": "system", "content": context},
+                {"role": "user", "content": Task}
+            ]
+        )
+        response = response.choices[0].message.content
+
+        #cas #imena so mela smisel - ne vec - ponovno majo, cas ga nima
+        """
+        e_time = time.time()
+        r_time = e_time - s_time
+        rounded_time_difference = round(r_time.total_seconds())
+
+        token_count = tokens_out(response, token_length=4)
+        print(f"Number of tokens (received): {token_count}, v {r_time} sekundah\n")
+        """
+        
+        match = re.search(r"```(?:\bpython\b)?(.*?)```", response, re.DOTALL)
+
+        #pattern in string? #0
+        if match:
+            r_code = match.group(1)
+        else:
+            raise ValueError(f"No python code found in the input string{response}")
+        
+        #zapis kode
+        p_file_Path = "/Users/timzav/Desktop/prak/tplot.py"
+        with open(p_file_Path, "w") as r_file:
+            r_file.write(r_code)
+        
+        # Check za ERROR
+        try:
+            result = subprocess.run(['/Users/timzav/miniconda3/bin/python', p_file_Path], stderr=subprocess.PIPE, text=True)
+
+            #R1
+            if result.returncode != 0:
+                e = str(result.stderr)
+                print(f"R1:{skupek_imen}")#Error in python script execution: {e}
+                task = f'''This code:'{response}, generated only this images:'{skupek_imen()}' and gave this error:'{str(e)}' generate code for only the rest of images that didn't get to be created by given code.'''
+                st = st + 1 
+            else:
+                print(f"Python script executed successfully in {st} attempt")
+                break
+            #R2  
+        except subprocess.CalledProcessError as e:
+            print(f"R2:{skupek_imen}")#Tryes:'{st}'.Python script crashed with error: '{e.stderr}'
+            task = f'''This code:'{response}, generated only this images:'{skupek_imen()}' and gave this error:'{str(e)}' generate code for only the rest of images that didn't get to be created by given code.'''
+            st = st + 1
+            #R3
+        except Exception as e:
+            print(f"R3:{skupek_imen}")#f"Tryes:'{st}'. An unexpected error occurred: '{e}'
+            task = f'''This code:'{response}, generated only this images:'{skupek_imen()}' and gave this error:'{str(e)}' generate code for only the rest of images that didn't get to be created by given code.'''
+            st = st + 1
+
+    else:
+
+        print(f"Attempts:'{st}'. Gave up at 4rd. imena\n:{skupek_imen()}")
+        print(f"token out:{tokens_out(response, token_length=4)}")
+        print(f"{tokens_in(Task, context)}")
+        break
+     
+
+
+#upload ciscenje-folder 
+    #safety
+folder_path = '/Users/timzav/Desktop/prak/uploads'
+if os.path.exists(folder_path):
+    file_paths = glob.glob(os.path.join(folder_path, '*'))
+    for file_path in file_paths:
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+#varnostno
+#tplot ciscenje-datoteka
+with open(p_file_Path, 'w'):
+   pass
+
